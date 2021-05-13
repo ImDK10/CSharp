e9c3f51898661b0d4fdfb329e135504864055cfe
ImDK10 <70590229+ImDK10@users.noreply.github.com>
2021-05-14 00:26:14 +0530

    C#

---
+++ RevStr.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppPali
+{
+    class Program
+    {
+        static void ReverseString(string s)
+        {
+            for (int i = s.Length; i >= 1; i--)
+            {
+                char x = s[i - 1];
+                string s1 = Convert.ToString(x);
+                Console.Write(s1);
+            }
+            Console.ReadLine();
+        }
+        static void Main(string[] args)
+        {
+
+            string s;
+            Console.WriteLine("Enter a String: ");
+            s = Console.ReadLine();
+
+            ReverseString(s);
+        }
+    }
+}
