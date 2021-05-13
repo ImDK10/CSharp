dccc86511300cb83d3c56351de29b4a3a762ad1b
ImDK10 <70590229+ImDK10@users.noreply.github.com>
2021-05-14 00:20:31 +0530

    C#

---
+++ BreakTheString.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppSeparateTheString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s;
+            Console.WriteLine("Enter a string:");
+            s = Console.ReadLine();
+
+            for(int i = 0; i< s.Length; i++)
+            {
+                if(s[i] == ' ')
+                {
+                    Console.WriteLine();
+                    continue;
+                }
+                Console.Write(s[i]);
+            }
+            Console.ReadLine();
+
+            /*var parts = s.Split(' ');
+            Array.ForEach(parts, Console.WriteLine);
+            Console.ReadLine();*/
+        }
+      
+    }
+}
