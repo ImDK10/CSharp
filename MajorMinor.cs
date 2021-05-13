29e357d00356f51001e17281db467573a38845a9
ImDK10 <70590229+ImDK10@users.noreply.github.com>
2021-05-14 00:22:16 +0530

    C#

---
+++ MajorMinor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppMajorMinor
+{
+    class Program
+    {
+        static void MajorMinor(int x)
+        {
+            if(x<=19)
+            {
+                Console.WriteLine("Minor");
+            }
+            else
+            {
+                Console.WriteLine("Major");
+            }
+            Console.ReadLine();
+        }
+        static void Main(string[] args)
+        {
+            string x;
+            Console.WriteLine("Enter Age Of the Person: ");
+            x = Console.ReadLine();
+
+            int Age = Convert.ToInt32(x);
+
+            MajorMinor(Age);
+        }
+    }
+}
